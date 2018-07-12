f19289f0330b25dbe0f956501cb7a3b04c899242
Jay Khan <jkhan992@gmail.com>
2018-07-12 22:08:35 +0100

    Refactor: added some comments, updated html

--- jasmine/jasmine-standalone-3.1.0/src/interface.js
+++ jasmine/jasmine-standalone-3.1.0/src/interface.js
@@ -30,10 +30,13 @@ $(document).ready(function() {
     updateTemperature();
   });
 
-  // have put updateTemperature into its own method because it is
-  // repeatedly called in each of the above calls
+  // have put updateTemperature into its own function (method( because
+  // it is repeatedly invoked (called) in each of the above functions
   function updateTemperature() {
     $('#current_temp').text(thermostat.temperature);
+
+    // change thre css style based on what is returned
+    // by getCurrentEnergyUsuage
     if(thermostat.getCurrentEnergyUsuage() === 'low-usage') {
       $('#current_temp').css('color', 'green')
     } else if(thermostat.getCurrentEnergyUsuage() === 'medium-usage') {

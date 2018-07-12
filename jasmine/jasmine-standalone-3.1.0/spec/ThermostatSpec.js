9ba65e38b9a7e07e6b407fdf7f4982c539578661
Jay Khan <jkhan992@gmail.com>
2018-07-12 11:58:11 +0100

    Feature: Limiting temperature and tests

--- jasmine/jasmine-standalone-3.1.0/spec/ThermostatSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/ThermostatSpec.js
@@ -22,16 +22,27 @@ describe('Thermostat test: ', function(){
   it('Increase temperature with up function', function() {
     // there are () after 'getCurrentTemperature' because we are
     // testing what this method is returning
-    thermostat.up(10);
-    expect(thermostat.getCurrentTemperature()).toEqual(30);
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
   it('Decrease temperature with down function', function() {
     // there are () after 'getCurrentTemperature' because we are
     // testing what this method is returning
-    thermostat.down(10);
-    expect(thermostat.getCurrentTemperature()).toEqual(10);
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 
+  it('Decrease temperature with down function a minimum of 10 degrees',
+    function() {
+    // there are () after 'getCurrentTemperature' because we are
+    // testing what this method is returning
+
+    // this is a for loop block, to loop 10 times to decrease the temperature
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
 
 });
--- jasmine/jasmine-standalone-3.1.0/src/Thermostat.js
+++ jasmine/jasmine-standalone-3.1.0/src/Thermostat.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function Thermostat(){
+  this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
 }
 
@@ -8,10 +9,32 @@ Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
 };
 
-Thermostat.prototype.up = function(number) {
-  this.temperature = this.temperature + number;
+// Thermostat up and down method which takes an argument
+// to increase/ decrease temperature
+
+// Thermostat.prototype.up = function(number) {
+//   this.temperature = this.temperature + number;
+// };
+//
+// Thermostat.prototype.down = function(number) {
+//   this.temperature = this.temperature - number;
+// };
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
 };
 
-Thermostat.prototype.down = function(number) {
-  this.temperature = this.temperature - number;
+Thermostat.prototype.down = function() {
+  // this if block is a guard statement, it checks if the current temperature
+  // is not below the minimum temperature
+  if (this.minimumTemperature()) {
+    return;
+  }
+  this.temperature -= 1;
+};
+
+// this method checks '===' if the current temperature is equal to the
+// minimum temperature and returns a boolean value 'true' or 'false'
+Thermostat.prototype.minimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
 };
